86498e29a33d0c19dc69d58ed9feda987d585e51
thomasaudo <thomas.audo@imt-atlantique.net>
2020-09-30 16:06:00 +0200

    [REFACTO] Wind

--- aeroport/Wind/src/wind.go
+++ aeroport/Wind/src/wind.go
@@ -18,17 +18,19 @@ func main() {
 	d := 200*time.Millisecond
 
 	for x := range time.Tick(d) {
-
 		fmt.Println(x)
-
-
-		c.Publish("aeroport/wind", 1, false, "20KM/h")
-
-		if token := c.Publish("aeroport/wind", 1, false, "20KM/h"); token.Wait() && token.Error() != nil {
-			fmt.Println(token.Error())
-		}
-		
-		fmt.Println("Data sent")
+		sendWind(c)
 	}
 
+}
+
+func sendWind(c mqtt.Client) {
+			
+	c.Publish("aeroport/wind", 1, false, "20KM/h")
+
+	if token := c.Publish("aeroport/wind", 1, false, "20KM/h"); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+	}
+	
+	fmt.Println("Data sent")
 }
